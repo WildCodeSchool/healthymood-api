4d62b7aed334536f1c89d13176f78e70102a1ca7
Amélie <ameliechevalier3006@gmail.com>
2020-06-17 15:56:30 +0200

    test pass for GET

---
+++ controllers/user.controller.js
@@ -0,0 +1,122 @@
+const User = require('../models/user.model.js');
+
+class UsersController {
+  static async create (req, res) {
+    if (!req.body) {
+      return res
+        .status(400)
+        .send({ errorMessage: 'Content can not be empty!' });
+    }
+
+    if (!req.body.username) {
+      return res.status(400).send({ errorMessage: 'Username can not be empty!' });
+    }
+
+    try {
+      const user = new User(req.body);
+      if (await User.usernameAlreadyExists(user.username)) {
+        res.status(400).send({
+          errorMessage: 'A user with this username already exists !'
+        });
+      } else if (User.emailAlreadyExists(user.email)) {
+        res.status(400).send({
+          errorMessage: 'A user with this email already exists !'
+        });
+      } else {
+        const data = await User.create(user);
+        res.status(201).send({ data });
+      }
+    } catch (err) {
+      res.status(500).send({
+        errorMessage:
+          err.message || 'Some error occurred while creating the user.'
+      });
+    }
+  }
+
+  static async findAll (req, res) {
+    try {
+      const data = (await User.getAll())
+        .map((u) => new User(u))
+        .map((u) => ({
+          id: u.id,
+          firstname: u.firstname,
+          lastname: u.lastname,
+          username: u.username,
+          email: u.email,
+          password: 'secret',
+          fb_uid: u.fb_uid,
+          avatar: u.avatar,
+          is_admin: u.is_admin,
+          blocked: u.blocked,
+          address_id: u.address_id
+        }));
+      res.send({ data });
+    } catch (err) {
+      res.status(500).send({
+        errorMessage:
+          err.message || 'Some error occurred while retrieving users.'
+      });
+    }
+  }
+
+  static async findOne (req, res) {
+    try {
+      const data = await User.findById(req.params.id);
+      res.send({ data });
+    } catch (err) {
+      if (err.kind === 'not_found') {
+        res.status(404).send({
+          errorMessage: `User with id ${req.params.id} not found.`
+        });
+      } else {
+        res.status(500).send({
+          errorMessage: 'Error retrieving user with id ' + req.params.id
+        });
+      }
+    }
+  }
+
+  static async update (req, res) {
+    if (!req.body) {
+      res.status(400).send({ errorMessage: 'Content can not be empty!' });
+    }
+
+    try {
+      const data = await User.updateById(
+        req.params.id,
+        new User(req.body)
+      );
+      res.send({ data });
+    } catch (err) {
+      if (err.kind === 'not_found') {
+        res.status(404).send({
+          errorMessage: `User with id ${req.params.id} not found.`
+        });
+      } else {
+        res.status(500).send({
+          errorMessage: 'Error updating user with id ' + req.params.id
+        });
+      }
+    }
+  }
+
+  static async delete (req, res) {
+    try {
+      await User.remove(req.params.id);
+      res.send({ message: 'User was deleted successfully!' });
+    } catch (err) {
+      if (err.kind === 'not_found') {
+        res.status(404).send({
+          errorMessage: `Not found User with id ${req.params.id}.`
+        });
+      } else {
+        res.status(500).send({
+          errorMessage: 'Could not delete User with id ' + req.params.id
+        });
+      }
+    }
+  }
+}
+
+module.exports = UsersController;
---
+++ tests/users-endpoints.test.js
@@ -0,0 +1,75 @@
+const request = require('supertest');
+const app = require('../server.js');
+const User = require('../models/user.model.js');
+
+describe('users endpoints', () => {
+  describe('GET /users', () => {
+    describe('when there are two users in DB', () => {
+      let res;
+      beforeEach(async () => {
+        await Promise.all([
+          User.create({
+            username: 'ana',
+            email: 'ana@gmail.cwo'
+          }),
+          User.create({
+            username: 'ana2',
+            email: 'ana@gmail.co'
+          })
+        ]);
+        res = await request(app).get('/users');
+      });
+
+      it('status is 200', async () => {
+        expect(res.status).toBe(200);
+      });
+
+      it('the returned data is an array containing two elements', async () => {
+        expect(Array.isArray(res.body.data));
+        expect(res.body.data.length).toBe(2);
+      });
+    });
+  });
+
+  xdescribe('POST /users', () => {
+    describe('when a valid payload is sent', () => {
+      let res;
+      beforeAll(async () => {
+        res = await request(app).post('/users').send({
+          username: 'ana3',
+          email: 'ana3@gmail.co'
+        });
+      });
+
+      it('returns 201 status', async () => {
+        expect(res.statusCode).toEqual(201);
+      });
+
+      it('returns the id of the created user', async () => {
+        expect(res.body.data).toHaveProperty('id');
+      });
+    });
+
+    describe('when a user with the same username already exists in DB', () => {
+      let res;
+      beforeAll(async () => {
+        User.create({
+          username: 'ana',
+          email: 'ana3@gmail.co'
+        });
+        res = await request(app).post('/users').send({
+          username: 'ana',
+          email: 'ana3@gmail.co'
+        });
+      });
+
+      it('returns a 400 status', async () => {
+        expect(res.status).toBe(400);
+      });
+
+      it('retuns an error message', async () => {
+        expect(res.body).toHaveProperty('errorMessage');
+      });
+    });
+  });
+});
