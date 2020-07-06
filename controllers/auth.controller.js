2a32e9e715c58098e9f3d5b7c2f92c6686930a76
tmicka23 <mickathp42@gmail.com>
2020-07-06 16:20:26 +0200

    no set a token for a blocked user

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,8 +3,16 @@ const User = require('../models/user.model.js');
 class authController {
   static async login (req, res) {
     const clientPayload = req.body;
+    const user = await User.findByEmail(clientPayload.email);
+    if (user.blocked) {
+      return res.status(403).send({ errorMessage: 'This user is blocked' });
+    }
+
     try {
-      const { token } = await User.login(clientPayload.email, clientPayload.password);
+      const { token } = await User.login(
+        clientPayload.email,
+        clientPayload.password
+      );
       res.status(200).send({ token });
     } catch (err) {
       res.status(400).send({ message: 'invalid credentials' });
