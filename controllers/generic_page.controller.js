1844b7385a93ef1d2f7244cfeaca0498eca8ae3d
tmicka23 <mickathp42@gmail.com>
2020-07-07 17:39:15 +0200

    wip generic pages update

--- controllers/generic_page.controller.js
+++ controllers/generic_page.controller.js
@@ -4,16 +4,18 @@ class GenericPagesController {
   static async create (req, res) {
     if (!req.body) {
       return res
-        .status(400)
-        .send({ errorMessage: 'Content can not be empty!' });
+      .status(400)
+      .send({ errorMessage: 'Content can not be empty!' });
     }
 
     if (!req.body.slug) {
       return res.status(400).send({ errorMessage: 'slug can not be empty!' });
     }
 
+    const user_id = req.currentUser.id
+    console.log(user_id)
     try {
-      const genericPage = new GenericPage(req.body);
+      const genericPage = new GenericPage({...req.body, user_id: user_id});
       if (await GenericPage.nameAlreadyExists(genericPage.slug)) {
         res.status(400).send({
           errorMessage: 'An genericPage with this slug already exists !'
@@ -25,28 +27,29 @@ class GenericPagesController {
     } catch (err) {
       res.status(500).send({
         errorMessage:
-          err.message || 'Some error occurred while creating the genericPage.'
+        err.message || 'Some error occurred while creating the genericPage.'
       });
     }
   }
 
   static async findAll (req, res) {
+
     try {
       const data = (await GenericPage.getAll())
-        .map((i) => new GenericPage(i))
-        .map((i) => ({
-          id: i.id,
-          title: i.title,
-          slug: i.slug,
-          published: i.published,
-          content: i.content,
-          user_id: i.user_id
-        }));
+      .map((i) => new GenericPage(i))
+      .map((i) => ({
+        id: i.id,
+        title: i.title,
+        slug: i.slug,
+        published: i.published,
+        content: i.content,
+        user_id: i.user_id
+      }));
       res.send({ data });
     } catch (err) {
       res.status(500).send({
         errorMessage:
-          err.message || 'Some error occurred while retrieving genericPage.'
+        err.message || 'Some error occurred while retrieving genericPage.'
       });
     }
   }
@@ -72,12 +75,12 @@ class GenericPagesController {
     if (!req.body) {
       res.status(400).send({ errorMessage: 'Content can not be empty!' });
     }
-
+    const user_id = req.currentUser.id
     try {
       const data = await GenericPage.updateById(
         req.params.id,
-        new GenericPage(req.body)
-      );
+        new GenericPage({...req.body, user_id: user_id})
+        );
       res.send({ data });
     } catch (err) {
       if (err.kind === 'not_found') {
