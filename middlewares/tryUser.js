5673b42daac72e2810d5336e611c4a0347250b91
Riri Z <riri.zamor@gmail.com>
2020-07-08 09:31:09 +0200

    factorisation du middleware

--- middlewares/tryUser.js
+++ middlewares/tryUser.js
@@ -3,15 +3,9 @@ const User = require('../models/user.model.js');
 module.exports = async (req, res, next) => {
   const decodedToken = req.token;
 
-  if (!decodedToken) {
-    return res.sendStatus(401);
-  } else {
+  if (decodedToken) {
     const userRecord = await User.findById(decodedToken.id);
     req.currentUser = userRecord;
-    if (!userRecord) {
-      return res.sendStatus(401);
-    } else {
-      return next();
-    }
   }
+  return next();
 };
