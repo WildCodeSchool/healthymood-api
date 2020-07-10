69e0313ffb27c0ce0632cbde48c504155c93184b
tmicka23 <mickathp42@gmail.com>
2020-07-10 11:53:24 +0200

    all routes are protected, articles_category tests works

--- routes/addresse.routes.js
+++ routes/addresse.routes.js
@@ -1,10 +1,12 @@
 const AddressesController = require('../controllers/addresse.controller.js');
 const router = require('express').Router();
+const requireAuth = require('../middlewares/requireAuth.js');
+const requireAdmin = require('../middlewares/requireAdmin.js');
 
-router.post('/', AddressesController.create);
+router.post('/', requireAuth, requireAdmin, AddressesController.create);
 router.get('/', AddressesController.findAll);
 router.get('/:id', AddressesController.findOne);
-router.patch('/:id', AddressesController.update);
-router.delete('/:id', AddressesController.delete);
+router.patch('/:id', requireAuth, requireAdmin, AddressesController.update);
+router.delete('/:id', requireAuth, requireAdmin, AddressesController.delete);
 
 module.exports = router;
--- routes/recipe.routes.js
+++ routes/recipe.routes.js
@@ -1,10 +1,12 @@
 const recipesController = require('../controllers/recipe.controller.js');
 const router = require('express').Router();
+const requireAuth = require('../middlewares/requireAuth.js');
+const requireAdmin = require('../middlewares/requireAdmin.js');
 
-router.post('/', recipesController.create);
+router.post('/', requireAuth, requireAdmin, recipesController.create);
 router.get('/', recipesController.findAll);
 router.get('/:id', recipesController.findOne);
-router.patch('/:id', recipesController.update);
-router.delete('/:id', recipesController.delete);
+router.patch('/:id', requireAuth, requireAdmin, recipesController.update);
+router.delete('/:id', requireAuth, requireAdmin, recipesController.delete);
 
 module.exports = router;
