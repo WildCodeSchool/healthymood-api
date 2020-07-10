8c195245a3e21074d530a4cba5f9c51f95e50fff
tmicka23 <mickathp42@gmail.com>
2020-07-10 12:23:01 +0200

    recipes-categories tests works

--- tests/recipe-categories-endpoints.test.js
+++ tests/recipe-categories-endpoints.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 const app = require('../server.js');
 const RecipesCategory = require('../models/recipe-categories.model.js');
+const { authenticateHelper } = require('../helpers/authenticateHelper');
 
 describe('recipe-categories endpoints', () => {
   describe('GET /recipe-categories', () => {
@@ -26,12 +27,32 @@ describe('recipe-categories endpoints', () => {
   });
 
   describe('POST /recipe-categories', () => {
-    describe('when a valid payload is sent', () => {
+    describe('when a user is not authenticated on admin', () => {
       let res;
       beforeAll(async () => {
-        res = await request(app).post('/recipe_categories').send({
+        res = await request(app).post('/dish_types').send({
           name: 'dessert'
         });
+      });
+
+      it('returns 401 status', async () => {
+        expect(res.statusCode).toEqual(401);
+      });
+    });
+    describe('when a valid payload is sent', () => {
+      let res;
+      let token;
+      beforeAll(async () => {
+        token = await authenticateHelper({
+          blocked: false,
+          isAdmin: true
+        });
+        res = await request(app)
+          .post('/recipe_categories')
+          .set('Authorization', `Bearer ${token}`)
+          .send({
+            name: 'dessert'
+          });
       });
 
       it('returns 201 status', async () => {
@@ -45,13 +66,21 @@ describe('recipe-categories endpoints', () => {
 
     describe('when a recipe-categories with the same name already exists in DB', () => {
       let res;
+      let token;
       beforeAll(async () => {
+        token = await authenticateHelper({
+          blocked: false,
+          isAdmin: true
+        });
         await RecipesCategory.create({
           name: 'entrée'
         });
-        res = await request(app).post('/recipe_categories').send({
-          name: 'entrée'
-        });
+        res = await request(app)
+          .post('/recipe_categories')
+          .set('Authorization', `Bearer ${token}`)
+          .send({
+            name: 'entrée'
+          });
       });
 
       it('returns a 400 status', async () => {
