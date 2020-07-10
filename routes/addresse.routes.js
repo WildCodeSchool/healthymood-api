916d9fe67f9bf469c5a8fa2fe5bd763ce51a60c0
tmicka23 <mickathp42@gmail.com>
2020-07-10 12:37:23 +0200

    all tests pass

--- routes/addresse.routes.js
+++ routes/addresse.routes.js
@@ -4,8 +4,8 @@ const requireAuth = require('../middlewares/requireAuth.js');
 const requireAdmin = require('../middlewares/requireAdmin.js');
 
 router.post('/', requireAuth, requireAdmin, AddressesController.create);
-router.get('/', AddressesController.findAll);
-router.get('/:id', AddressesController.findOne);
+router.get('/', requireAuth, requireAdmin, AddressesController.findAll);
+router.get('/:id', requireAuth, AddressesController.findOne);
 router.patch('/:id', requireAuth, requireAdmin, AddressesController.update);
 router.delete('/:id', requireAuth, requireAdmin, AddressesController.delete);
 
