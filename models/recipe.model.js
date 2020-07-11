f85041438198299ba6de21ec39fa5d98a38723e3
Amélie <ameliechevalier3006@gmail.com>
2020-07-11 14:44:53 +0200

    try to understand why SQL request doesn't work on insomnia

--- models/recipe.model.js
+++ models/recipe.model.js
@@ -59,17 +59,21 @@ class Recipe {
   }
 
   static async findByKeyWord (query) {
+    console.log(query.meal_types)
     const keyword = query.search ? query.search : '';
     const mealTypes = query.meal_types ? query.meal_types : [];
-    const sqlMealTypes = mealTypes.map(mealtype => parseInt(mealtype)).toString();
+    const sqlMealTypes = query.meal_types ? mealTypes.map(mealtype => parseInt(mealtype)).toString() : [];
     const sqlKeyword = query.search ? `%${keyword}%` : '';
     console.log(sqlKeyword);
     console.log(sqlMealTypes);
 
+    const request = `SELECT DISTINCT recipes.name FROM meal_type_recipes INNER JOIN recipes ON meal_type_recipes.recipe_id = recipes.id WHERE meal_type_recipes.meal_type_id IN (${sqlMealTypes})`
+    console.log(request)
+
     return db.query(
 
-      'SELECT DISTINCT recipes.name FROM meal_type_recipes INNER JOIN recipes ON meal_type_recipes.recipe_id = recipes.id WHERE meal_type_recipes.meal_type_id IN (?) UNION SELECT name FROM recipes WHERE recipes.name LIKE ? OR recipes.content LIKE ?',
-      [sqlMealTypes, sqlKeyword, sqlKeyword]
+      'SELECT DISTINCT recipes.name FROM meal_type_recipes INNER JOIN recipes ON meal_type_recipes.recipe_id = recipes.id WHERE meal_type_recipes.meal_type_id IN (?)', //  UNION SELECT name FROM recipes WHERE recipes.name LIKE ? OR recipes.content LIKE ?
+      [sqlMealTypes]
     );
   }
 
